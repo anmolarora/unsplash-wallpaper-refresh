8dca5ef8f97575caab9c2a353ac9317812207090
Anmol Arora <anmol.arora@adwyze.com>
2017-10-28 14:54:37 +0530

    Init

---
+++ app/controllers/image_urls_controller.rb
@@ -0,0 +1,5 @@
+class ImageUrlsController < ApplicationController
+  def random
+    
+  end
+end
---
+++ config/initializers/unsplash.rb
@@ -0,0 +1,4 @@
+Unsplash.configure do |config|
+  config.application_id     = ENV['UNSPLASH_APP_ID']
+  config.application_secret = ENV['UNSPLASH_APP_SECRET']
+end
